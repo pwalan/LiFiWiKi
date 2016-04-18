9cab5ed38cc7748f0f8ea7d65e8615f074e9be3a
pwalan <pwalan@qq.com>
2016-04-18 21:03:46 +0800

    some change

--- app/src/main/java/lab414/bupt/com/lifiwiki/video_list.java
+++ app/src/main/java/lab414/bupt/com/lifiwiki/video_list.java
@@ -2,6 +2,7 @@ package lab414.bupt.com.lifiwiki;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Handler;
 import android.os.Message;
 import android.support.v7.app.ActionBarActivity;
@@ -9,14 +10,21 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import lab414.bupt.com.lifiwiki.utils.C;
 
@@ -29,6 +37,8 @@ public class video_list extends Activity {
 
     Handler handler;
 
+    private ListView videolist;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,25 +47,42 @@ public class video_list extends Activity {
         Intent intent = getIntent();
         things = intent.getStringExtra("things");
 
+        videolist = (ListView) findViewById(R.id.videolist);
 
         handler = new Handler() {
             @Override
             public void handleMessage(Message msg) {
                 if(msg.what == 0x123) {
-                    final TextView show = (TextView) findViewById(R.id.show);
+                    List<Map<String, String>> listItems = new ArrayList<Map<String, String>>();
                     if(response != null) {
+                        //读取response中的数据
                         try {
-                            show.setText(response.toString()+"\n");
                             JSONArray jarray=new JSONArray(response.get("message").toString());
                             Log.d("Debug",jarray.toString());
                             for(int i=0;i<jarray.length();i++){
+                                Map<String, String> listItem = new HashMap<String, String>();
                                 JSONObject jobject=jarray.getJSONObject(i);
-                                show.append("username："+jobject.get("username")
-                                +"\nurl："+jobject.get("url")+"\n");
+                                listItem.put("username", jobject.get("username").toString());
+                                listItem.put("url", jobject.get("video").toString());
+                                listItems.add(listItem);
                             }
                         } catch (Exception e) {
                             e.printStackTrace();
                         }
+                        SimpleAdapter adapter = new SimpleAdapter(video_list.this, listItems, R.layout.simple_item,
+                                new String[]{"username", "url"},
+                                new int[]{R.id.name, R.id.url});
+                        videolist.setAdapter(adapter);
+                        videolist.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                            @Override
+                            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                                Intent intent = new Intent(Intent.ACTION_VIEW);
+                                String type = "video/* ";
+                                Uri uri = Uri.parse(listItems.get(position).get("url"));
+                                intent.setDataAndType(uri, type);
+                                startActivity(intent);
+                            }
+                        });
                     }
                 }
             }
