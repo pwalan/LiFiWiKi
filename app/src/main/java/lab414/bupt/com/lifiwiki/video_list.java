448c89cf101a06181cf42f232197c7f79c472849
pwalan <pwalan@qq.com>
2016-04-18 21:41:49 +0800

    some change

--- app/src/main/java/lab414/bupt/com/lifiwiki/video_list.java
+++ app/src/main/java/lab414/bupt/com/lifiwiki/video_list.java
@@ -10,12 +10,15 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.VideoView;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -39,6 +42,10 @@ public class video_list extends Activity {
 
     private ListView videolist;
 
+    private VideoView videoView;
+
+    private List<Map<String, String>> listItems;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -49,11 +56,22 @@ public class video_list extends Activity {
 
         videolist = (ListView) findViewById(R.id.videolist);
 
+        videoView=(VideoView)findViewById(R.id.videoView);
+        videoView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                videoView.stopPlayback();
+                videoView.setVisibility(View.INVISIBLE);
+                return false;
+            }
+        });
+
+
         handler = new Handler() {
             @Override
             public void handleMessage(Message msg) {
                 if(msg.what == 0x123) {
-                    List<Map<String, String>> listItems = new ArrayList<Map<String, String>>();
+                    listItems = new ArrayList<Map<String, String>>();
                     if(response != null) {
                         //读取response中的数据
                         try {
@@ -62,27 +80,26 @@ public class video_list extends Activity {
                             for(int i=0;i<jarray.length();i++){
                                 Map<String, String> listItem = new HashMap<String, String>();
                                 JSONObject jobject=jarray.getJSONObject(i);
-                                listItem.put("username", jobject.get("username").toString());
+                                listItem.put("username", "用户名："+jobject.get("username").toString()+"  视频地址：");
                                 listItem.put("url", jobject.get("video").toString());
                                 listItems.add(listItem);
                             }
+                            SimpleAdapter adapter = new SimpleAdapter(video_list.this, listItems, R.layout.simple_item,
+                                    new String[]{"username", "url"},
+                                    new int[]{R.id.name, R.id.url});
+                            videolist.setAdapter(adapter);
+                            videolist.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                                @Override
+                                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                                    Toast.makeText(video_list.this, "你点击了第" + (position+1) + "个视频", Toast.LENGTH_SHORT).show();
+                                    videoView.setVideoURI(Uri.parse(listItems.get(position).get("url")));
+                                    videoView.setVisibility(View.VISIBLE);
+                                    videoView.start();
+                                }
+                            });
                         } catch (Exception e) {
                             e.printStackTrace();
                         }
-                        SimpleAdapter adapter = new SimpleAdapter(video_list.this, listItems, R.layout.simple_item,
-                                new String[]{"username", "url"},
-                                new int[]{R.id.name, R.id.url});
-                        videolist.setAdapter(adapter);
-                        videolist.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                            @Override
-                            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                                Intent intent = new Intent(Intent.ACTION_VIEW);
-                                String type = "video/* ";
-                                Uri uri = Uri.parse(listItems.get(position).get("url"));
-                                intent.setDataAndType(uri, type);
-                                startActivity(intent);
-                            }
-                        });
                     }
                 }
             }
